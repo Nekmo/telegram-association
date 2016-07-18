bc110daf087b23ea3329b4ce39255e9fe186bdbd
Nekmo <contacto@nekmo.com>
2016-07-18 23:35:10 +0200

    IGNORE_MESSAGES_OFFTIME.

--- telegram_association/utils/telegram.py
+++ telegram_association/utils/telegram.py
@@ -1,13 +1,16 @@
 import time
 
 
+IGNORE_MESSAGES_OFFTIME = 60
+
+
 def get_name(name_data):
     return ' '.join(filter(lambda x: x, [name_data.first_name, name_data.last_name]))
 
 
 def securize_message(fn):
     def wrapper(message, *args, **kwargs):
-        if time.time() - message.date > 10:
+        if time.time() - message.date > IGNORE_MESSAGES_OFFTIME:
             return
         return fn(message, *args, **kwargs)
     return wrapper
